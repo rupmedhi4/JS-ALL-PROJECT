09112162ebc6b501597f64afe3ed8aa01e71529e
Rup medhi <rupmmedhi@gmail.com>
2023-09-01 23:49:09 +0530

    guess number game

---
+++ Peoject_3/script.js
@@ -0,0 +1,64 @@
+const guessNumber = document.querySelector(".guessField")
+const submitBtn = document.querySelector(".guessSubmit")
+const previousGuess = document.querySelector(".guesses")
+const Guess_Remaining = document.querySelector(".lastResult")
+const resultDiv = document.querySelector(".resultParas")
+const gameStart = document.querySelector(".gameOverPara")
+const number_random = document.querySelector(".random_number")
+
+
+
+let numberOfGuess = 10
+let guessCount = []
+let p = document.createElement("p")
+
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (numberOfGuess === 0) {
+        endGame()
+    } else if(guessNumber.value === '' || isNaN(guessNumber.value) || parseInt(guessNumber.value) < 0) {
+      alert("please enter a valid number")
+    }else{
+        guessCount.push(guessNumber.value)
+        previousGuess.innerHTML = `${guessCount}`
+        numberOfGuess = numberOfGuess - 1
+        Guess_Remaining.innerHTML = `${numberOfGuess}`
+      
+        checkGuess()
+    }
+
+})
+
+const checkGuess = () => {
+    const randomNumber = Math.floor(Math.random() * 100 + 1)
+   
+    if( parseInt(guessNumber.value) === randomNumber)  alert("Congrasulation You are win same number you guess")
+    number_random.innerHTML =`Random number is : ${randomNumber}`
+}
+
+const endGame = ()=>{
+    guessNumber.setAttribute("disabled", "")
+    submitBtn.setAttribute("disabled", "")
+    guessNumber.value =''
+    let text = document.createTextNode("your game is over please restart your game")
+    p.classList.add("gameOverPara")
+    p.style.cursor="pointer"
+    p.appendChild(text)
+    resultDiv.appendChild(p)
+
+}
+
+
+
+p.addEventListener("click", (e)=>{
+    e.preventDefault()
+    guessNumber.removeAttribute("disabled")
+    numberOfGuess=10
+    number_random.innerHTML = null
+     guessCount =[]
+    previousGuess.innerHTML =  `${guessCount}`
+    Guess_Remaining.innerHTML = `${numberOfGuess}`
+    submitBtn.removeAttribute("disabled")
+   
+})
